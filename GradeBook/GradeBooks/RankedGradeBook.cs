72057306a2103b9003a00c8571e3d3d60d3db6c3
AdriaFLuna <adria.fernandez.luna@avanade.com>
2018-09-18 17:38:41 +0200

    4.1. Create CalculateStatistics Override v1

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -14,7 +14,7 @@ namespace GradeBook.GradeBooks
         }
         public override char GetLetterGrade(double averageGrade)
         {
-            if (Students.Count > 5)
+            if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked-grading requires a minimum of 5 students to work");
     
             var threshold = (int)Math.Ceiling(Students.Count * 0.2);
@@ -36,5 +36,25 @@ namespace GradeBook.GradeBooks
                 return 'F';
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+            base.CalculateStatistics();
+        }
+
+        public override void CalculateStudentStatistics(string name)
+        {
+            if(Students.Count < 5)
+            {
+                Console.WriteLine("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+                return;
+            }
+
+            base.CalculateStudentStatistics(name);
+        }
     }
 }
